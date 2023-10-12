7826c18945eb9d73c8f57b48cf11867bb42d07c1
iW90 <ingrid.wagner.dev@gmail.com>
2023-10-12 16:28:35 -0300

    fix: change print status validation

--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 20:34:26 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/12 15:17:32 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:23:49 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,24 @@ time_t	print_status(t_plate *philo, int status)
 {
 	time_t	time;
 
-	if (call_butler()->stop)
-		return (0);
 	time = get_time_in_usec();
 	pthread_mutex_lock(&call_butler()->printer);
-	printf("%li\t%i ", time / 1000, philo->id + 1);
-	if (status == DIED)
-		printf("is dead.\n");
-	if (status == EATING)
-		printf("is eating.\n");
-	if (status == SLEEPING)
-		printf("is sleeping.\n");
-	if (status == THINKING)
-		printf("is thinking.\n");
-	if (status == LEFT_HASHI)
-		printf("has taken a fork.\n");
-	if (status == RIGHT_HASHI)
-		printf("has taken a fork.\n");
+	if (!call_butler()->stop)
+	{
+		printf("%li\t%i ", time / 1000, philo->id + 1);
+		if (status == DIED)
+			printf("is dead.\n");
+		if (status == EATING)
+			printf("is eating.\n");
+		if (status == SLEEPING)
+			printf("is sleeping.\n");
+		if (status == THINKING)
+			printf("is thinking.\n");
+		if (status == LEFT_HASHI)
+			printf("has taken a fork.\n");
+		if (status == RIGHT_HASHI)
+			printf("has taken a fork.\n");
+	}
 	pthread_mutex_unlock(&call_butler()->printer);
 	return (time);
 }
